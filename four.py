909ab5911d520ad07f9f4bab46e9c05a115872ac
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-03-02 14:18:03 +0100

    Add files via upload

---
+++ four.py
@@ -0,0 +1,12 @@
+# def checkMultiples(m, count):
+#     for i in range(count):
+#         print(i*m)
+# checkMultiples(4, 10)
+
+
+a = int(input("enter a number: "))
+b = int(input("enter a count number: "))
+
+for i in range(b):
+    print(i*a)
+
